82e74dc3b290d9eb5fc454a2a3adbfc198749321
Haripriyan <hari@chargebeeinc.com>
2020-07-21 11:58:33 +0530

    feat: adds toggle to enable/disable error logging

--- chargebee/src/main/java/com/chargebee/android/Chargebee.kt
+++ chargebee/src/main/java/com/chargebee/android/Chargebee.kt
@@ -7,11 +7,13 @@ object Chargebee {
     var publishableApiKey: String = ""
     var encodedApiKey: String = ""
     var baseUrl: String = ""
+    var allowErrorLogging: Boolean = true
 
-    fun configure(site: String, publishableApiKey: String) {
+    fun configure(site: String, publishableApiKey: String, allowErrorLogging: Boolean = true) {
         this.publishableApiKey = publishableApiKey
         this.site = site
         this.encodedApiKey = Credentials.basic(publishableApiKey, "")
         this.baseUrl = "https://${site}.chargebee.com/api/"
+        this.allowErrorLogging = allowErrorLogging
     }
 }
--- chargebee/src/main/java/com/chargebee/android/loggers/CBLogger.kt
+++ chargebee/src/main/java/com/chargebee/android/loggers/CBLogger.kt
@@ -1,5 +1,6 @@
 package com.chargebee.android.loggers
 
+import com.chargebee.android.Chargebee
 import com.chargebee.android.models.ResultHandler
 import com.chargebee.android.resources.LogType
 import com.chargebee.android.resources.LoggerResource
@@ -8,13 +9,15 @@ internal class CBLogger(private val name: String,
             private val action: String) {
 
     fun error(message: String, code: Int? = null) {
-        ResultHandler.safeExecute({
-            LoggerResource().log(
-                action,
-                LogType.ERROR,
-                message,
-                code
-            )
-        }, {})
+        if (Chargebee.allowErrorLogging) {
+            ResultHandler.safeExecute({
+                LoggerResource().log(
+                    action,
+                    LogType.ERROR,
+                    message,
+                    code
+                )
+            }, {})
+        }
     }
 }
