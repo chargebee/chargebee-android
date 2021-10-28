c9e058f93683c1bdb3e28856f892ff09a0b3e7ac
amutha_c <amutha.chithuraj@external.santander.us>
2021-10-28 18:28:37 +0530

    Done : Added code to Confirm the subscription and Show purchase token on dialog.

--- app/src/main/java/com/chargebee/example/BaseActivity.java
+++ app/src/main/java/com/chargebee/example/BaseActivity.java
@@ -1,8 +1,13 @@
 package com.chargebee.example;
 
+import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class BaseActivity extends AppCompatActivity {
@@ -38,4 +43,19 @@ public class BaseActivity extends AppCompatActivity {
         progressDialog = null;
     }
 
+    protected void showPurchaseSuccessDialog(String purchaseToken){
+        final Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.dialog_layout);
+        TextView textViewMessage = (TextView) dialog.findViewById(R.id.tv_message);
+        String msg = getResources().getString(R.string.dialog_message)+" "+purchaseToken;
+        textViewMessage.setText(msg);
+        Button dialogButton = (Button) dialog.findViewById(R.id.dialogButtonOK);
+        dialogButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+    }
 }
