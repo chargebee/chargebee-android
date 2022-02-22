f9abfa58254a82a7cca6e9e98d5ee95641dea043
cb-amutha <amutha.c@chargebee.com>
2022-02-22 09:36:55 +0530

    Fixed: End points URL.

--- chargebee/src/main/java/com/chargebee/android/repository/AuthRepository.kt
+++ chargebee/src/main/java/com/chargebee/android/repository/AuthRepository.kt
@@ -7,12 +7,11 @@ import retrofit2.http.*
 
 interface AuthRepository {
     @FormUrlEncoded
-    @POST("{version}/in_app_details/{sdkKey}/verify_app_detail/")
+    @POST("v2/in_app_details/{sdkKey}/verify_app_detail/")
     suspend fun authenticateClient(
         @Header("Authorization") token: String = Chargebee.encodedApiKey,
         @Header("platform") platform: String = Chargebee.platform,
         @Header("sdkVersion") sdkVersion: String = Chargebee.sdkVersion,
-        @Path("version") version: String = Chargebee.version,
         @Path("sdkKey") sdkKey: String = Chargebee.sdkKey,
         @FieldMap data: Map<String, String>): Response<CBAuthResponse?>
 }
--- chargebee/src/main/java/com/chargebee/android/repository/PlanRepository.kt
+++ chargebee/src/main/java/com/chargebee/android/repository/PlanRepository.kt
@@ -12,21 +12,19 @@ import retrofit2.http.Query
 
 internal interface PlanRepository {
 
-    @GET("{version}/plans/{planId}")
+    @GET("v2/plans/{planId}")
     suspend fun retrievePlan(
         @Header("Authorization") token: String = Chargebee.encodedApiKey,
         @Header("platform") platform: String = Chargebee.platform,
         @Header("sdkVersion") sdkVersion: String = Chargebee.sdkVersion,
-        @Path("version") version: String = Chargebee.version,
         @Path("planId") planId: String
     ): Response<PlanWrapper?>
 
-    @GET("{version}/plans")
+    @GET("v2/plans")
     suspend fun retrieveAllPlans(
         @Header("Authorization") token: String = Chargebee.encodedApiKey,
         @Header("platform") platform: String = Chargebee.platform,
         @Header("sdkVersion") sdkVersion: String = Chargebee.sdkVersion,
-        @Path("version") version: String = Chargebee.version,
         @Query("sort_by[desc]") sort: String,
         @Query("channel") channel: String
     ): Response<PlansWrapper?>
