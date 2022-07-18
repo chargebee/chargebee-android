90f29e3466b865af3f80053d20a1b43c62da4244
cb-amutha <amutha.c@chargebee.com>
2022-07-18 12:42:45 +0530

    Removed unused http logging.

--- chargebee/src/main/java/com/chargebee/android/resources/BaseResource.kt
+++ chargebee/src/main/java/com/chargebee/android/resources/BaseResource.kt
@@ -3,8 +3,6 @@ package com.chargebee.android.resources
 import com.google.gson.FieldNamingPolicy
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -12,9 +10,6 @@ internal open class BaseResource(baseUrl: String) {
     var apiClient: Retrofit
 
     init {
-        val interceptor = HttpLoggingInterceptor()
-        interceptor.level = HttpLoggingInterceptor.Level.BODY
-        val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
 
         val gson: Gson = GsonBuilder()
             .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
@@ -22,7 +17,6 @@ internal open class BaseResource(baseUrl: String) {
 
         apiClient = Retrofit.Builder()
             .baseUrl(baseUrl)
-            //.client(client)
             .addConverterFactory(GsonConverterFactory.create(gson))
             .build()
     }
